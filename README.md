bb5cf2d7ea138e01a020a81a595491a78003ca20
SevenCatsHere <snowcolton@hotmail.com>
2020-06-03 21:21:05 -0400

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TripSit Monorepo
+
+
--- packages/web-main/compile-templates.js
+++ packages/web-main/compile-templates.js
@@ -1,22 +1,18 @@
 'use strict';
 
-const { promisify } = require('util');
-const fs = require('fs').promises;
 const path = require('path');
-const glob = promisify(require('glob'));
 const pug = require('pug');
+const pages = require('./pages');
 
 const basedir = path.resolve('templates');
 
-Promise.all([
-	glob('templates/*.md')
-		.then(mdFilePaths => Promise.all(mdFilePaths
-			.map(filepath => fs.readFile(filepath, 'utf-8')
-				.then(contents => ({
-					contents,
-					filename: path.parse(filepath.replace(/\.md$/, '.html')).base,
-				}))))),
-	fs.readFile(path.resolve('templates/about.pug'), 'utf-8'),
-])
-	.then(([files, template]) => Promise.all(files
-		.map(({ filename, contents }) => pug.compile(template, { filename, basedir, compileDebug: true })(contents))));
+Promise.all(pages.map(({
+	url,
+	content,
+	vars,
+	template = 'default',
+}) => pug.compileFile(path.resolve(`templates/${template}.pug`), { basedir })({
+	pageUrl: url,
+	contentFile: path.resolve(`content/${content}.md`),
+	...vars,
+})));
